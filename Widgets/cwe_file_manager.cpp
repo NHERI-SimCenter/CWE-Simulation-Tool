ff103f99f7be939878e22b058bc963e6371e5874
Peter Mackenzie <mackheln@gmail.com>
2017-06-21 23:13:27 -0700

    I first draft of the new CWE GUI
    * All widgets are implemented as defined by Max and Peter S.
    * cwe_mainwindow is the new application main window
    * stacked widgets are created at in the constructor
    KNOWN ISSUES:
    * the signal/slot mechanism is not yet working.  I tested to verify
    that all widgets are there (and they are), but the connect method
    doesn’t work as anticipated
    * no functionality implemented
    * Peter S.’ functionality got broken by the incomplete transition.

--- Widgets/cwe_file_manager.cpp
+++ Widgets/cwe_file_manager.cpp
@@ -6,6 +6,10 @@ CWE_file_manager::CWE_file_manager(QWidget *parent) :
     ui(new Ui::CWE_file_manager)
 {
     ui->setupUi(this);
+    QPixmap lpix(":/buttons/images/CWE_button_left_arrows.png");
+    ui->lbl_left_arrows->setPixmap(lpix);
+    QPixmap rpix(":/buttons/images/CWE_button_right_arrows.png");
+    ui->lbl_right_arrows->setPixmap(rpix);
 }
 
 CWE_file_manager::~CWE_file_manager()
