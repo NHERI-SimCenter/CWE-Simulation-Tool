fd1a122689291258338d62e304be76e3dcf911b7
Peter Mackenzie <mackheln@gmail.com>
2017-08-24 17:09:38 -0700

    work around for style sheet problem

--- cwe_guiWidgets/cwe_parametertab.cpp
+++ cwe_guiWidgets/cwe_parametertab.cpp
@@ -2,7 +2,7 @@
 #include "ui_cwe_parametertab.h"
 
 CWE_ParameterTab::CWE_ParameterTab(QWidget *parent) :
-    QWidget(parent),
+    QScrollArea(parent),
     ui(new Ui::CWE_ParameterTab)
 {
     ui->setupUi(this);
