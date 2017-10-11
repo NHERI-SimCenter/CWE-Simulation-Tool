709b999f1ec14dc57954340b05433ca68b859e51
Peter Mackenzie <mackheln@gmail.com>
2017-10-11 14:52:08 -0700

    connecting dynamic case selction

--- cwe_guiWidgets/cwe_create_copy_simulation.h
+++ cwe_guiWidgets/cwe_create_copy_simulation.h
@@ -2,6 +2,8 @@
 #define CWE_CREATE_COPY_SIMULATION_H
 
 #include <QFrame>
+#include <QMap>
+#include <QList>
 
 class FileOperator;
 
@@ -23,30 +25,23 @@ public:
 
 private slots:
     void on_lineEdit_newCaseName_editingFinished();
-
     void on_pBtn_cancel_clicked();
-
     void on_pBtn_create_copy_clicked();
-
     void on_tabWidget_currentChanged(int index);
-
     void on_radioButton_2Dshape_clicked();
-
     void on_radioButton_3Dshape_clicked();
-
     void on_radioButton_channelFlow_clicked();
-
     void on_pb_image_2Dshape_clicked();
-
     void on_pb_image_3Dshape_clicked();
-
     void on_pb_image_channelFlow_clicked();
+    void selectCaseTemplate();
 
 private:
     Ui::CWE_Create_Copy_Simulation *ui;
     void populateCaseTypes(QStringList &caseTypeFiles);
     SimulationType setSimulationType(SimulationType);
 
+    QMap<int, QList<QWidget *> > templateListMap;
 };
 
 #endif // CWE_CREATE_COPY_SIMULATION_H
