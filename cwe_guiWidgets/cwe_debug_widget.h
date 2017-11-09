c9f9056d572662bf3249dca146ed577258fdbbd9
Peter Sempolinski <sempolin@bc.edu>
2017-11-09 16:38:12 -0500

    Implemented Download/upload screen, untested, removed much extra code.

--- cwe_guiWidgets/cwe_debug_widget.h
+++ cwe_guiWidgets/cwe_debug_widget.h
@@ -2,7 +2,6 @@
 #define CWE_DEBUG_WIDGET_H
 
 #include <QWidget>
-#include "cwe_busyindicator.h"
 
 namespace Ui {
 class CWE_Debug_Widget;
@@ -16,15 +15,8 @@ public:
     explicit CWE_Debug_Widget(QWidget *parent = 0);
     ~CWE_Debug_Widget();
 
-private slots:
-    void on_pb_setBusy_clicked();
-    void on_pb_stopBusy_clicked();
-
-    void on_pb_timedBusy_clicked();
-
 private:
     Ui::CWE_Debug_Widget *ui;
-    CWE_BusyIndicator *theBusy;
 };
 
 #endif // CWE_DEBUG_WIDGET_H
