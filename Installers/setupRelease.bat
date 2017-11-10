42b8c38e44ea2ce1b239689cc5f332815beb820e
Peter Sempolinski <sempolin@bc.edu>
2017-11-10 16:52:53 -0500

    Significantly updated Windows install generator script to be cleaner and more generally useful

--- Installers/setupRelease.bat
+++ Installers/setupRelease.bat
@@ -1,23 +1,42 @@
 REM @ECHO OFF
 echo "Preparing redistributable package for 64-bit MSVC release"
 
+REM To Use:    1) Go to an empty folder for creating the install package
+REM 		2) Invoke this script, passing the folder of the compiled release
+REM 		Example: ..\qtProjects\CFDClientProgram\Installers\setupRelease.bat ..\qtProjects\build-CFDClientProgram-Desktop_Qt_5_9_2_MSVC2017_64bit-Release\release
+
+SET MADE_EXEC_DIR=%1
+SET GENERATOR_DIR=%~dp0
+
+rem ----- adjust user settings -----------------------------
+
+SET QT_WIN_DEPLOY=D:\QT\5.9.2\msvc2017_64\bin\windeployqt
+SET QT_BIN_CREATE=D:\QT\Tools\QtInstallerFramework\3.0\bin\binarycreator.exe
+
+rem ----- do not modify below here -------------------------
+
 RD /S /Q release
 RD /S /Q installer
 
 MKDIR release
 
-COPY ..\qtProjects\build-CFDClientProgram-Desktop_Qt_5_9_0_MSVC2017_64bit-Release\release\*.exe release\
+COPY %MADE_EXEC_DIR%\CFDClientProgram.exe release\
 
-set PATH=%PATH%;D:\QT\5.9\msvc2017_64\bin\
+%QT_WIN_DEPLOY% release\CFDClientProgram.exe --no-translations
 
-windeployqt release\CFDClientProgram.exe --no-translations
+MKDIR installer
+MKDIR installer\packages 
+MKDIR installer\config
 
-XCOPY /S /I dataStore installer
+XCOPY /S /I %GENERATOR_DIR%\dataStore\packages installer\packages
+XCOPY /S /I %GENERATOR_DIR%\dataStore\config installer\config
 
 XCOPY /S /I release\* installer\packages\nheri.simcenter.cfdclient\data
 
 cd installer
 
-D:\QT\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe -c config\config.xml -p packages setupSimCenterCFD.exe
+%QT_BIN_CREATE% --offline-only -c config\config.xml -p packages ../setupSimCenterCFD.exe
 
 PAUSE
+
+cd ..
