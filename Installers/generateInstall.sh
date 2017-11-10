54786587d08e4dc5e2da4f6f93b84a54f34acd01
Peter Sempolinski <sempolin@bc.edu>
2017-11-10 11:27:39 -0500

    Significantly updated linux install generator script to be cleaner and more generally useful

--- Installers/generateInstall.sh
+++ Installers/generateInstall.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+#To Use:    1) Go to an empty folder for creating the install package
+#           2) Invoke this script, passing the folder of the compiled release
+#           Example: ../qtProjects/CFDClientProgram/Installers/generateInstall.sh ../qtProjects/build-CFDClientProgram-Desktop_Qt_5_9_2_GCC_64bit-Release/
+
+MADE_EXEC_DIR=$1
+GENERATOR_DIR=$(dirname "$0")
+
+
 #----- adjust user settings -----------------------------
 
-QT_HOME=/home/psempoli/Qt/5.9/gcc_64/lib
-QT_PLUGINS=/home/psempoli/Qt/5.9/gcc_64/plugins
+QT_HOME=~/Qt/5.9.2/gcc_64/lib
+QT_PLUGINS=~/Qt/5.9.2/gcc_64/plugins
+QT_BIN_CREATE=~/Qt/Tools/QtInstallerFramework/3.0/bin/binarycreator
 
 #----- do not modify below here -------------------------
 
@@ -13,7 +22,7 @@ rm -rf installer
 mkdir release
 mkdir release/exec
 
-cp ../qtProjects/build-CFDClientProgram-Desktop_Qt_5_9_0_GCC_64bit-Release/CFDClientProgram release/exec
+cp $MADE_EXEC_DIR/CFDClientProgram release/exec
 
 mkdir release/plugins
 mkdir release/plugins/platforms
@@ -23,8 +32,8 @@ cp $QT_PLUGINS/platforms/libqxcb.so release/plugins/platforms
 cp $QT_PLUGINS/xcbglintegrations/libqxcb-egl-integration.so release/plugins/xcbglintegrations/libqxcb-egl-integration.so
 cp $QT_PLUGINS/xcbglintegrations/libqxcb-glx-integration.so release/plugins/xcbglintegrations/libqxcb-glx-integration.so
 
-cp dataStore/RunSimCenterCFD release/RunSimCenterCFD.sh
-
+cp $GENERATOR_DIR/dataStore/RunSimCenterCWE release/RunSimCenterCWE.sh
+chmod 744 release/RunSimCenterCWE.sh
 
 mkdir release/lib
 
@@ -41,10 +50,11 @@ cp $QT_HOME/libQt5DBus.so.5 release/lib
 
 mkdir installer
 
-cp -R dataStore/packages installer
-cp -R dataStore/config installer
+cp -R $GENERATOR_DIR/dataStore/packages installer
+cp -R $GENERATOR_DIR/dataStore/config installer
+mkdir installer/packages/nheri.simcenter.cfdclient/data
 cp -R release/* installer/packages/nheri.simcenter.cfdclient/data/
 
 cd installer
 
-/home/psempoli/Qt/Tools/QtInstallerFramework/2.0/bin/binarycreator --offline-only -c config/config.xml -p packages InstallSimCenterCFD
+$QT_BIN_CREATE --offline-only -c config/config.xml -p packages ../InstallSimCenterCFD
