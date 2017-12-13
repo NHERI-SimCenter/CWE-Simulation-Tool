4b79e9489258471d2ba5f4e42d3629cb908377de
Peter Sempolinski <sempolin@bc.edu>
2017-12-13 17:24:55 -0500

    Parameter screen w/ no login

--- cwe_guiWidgets/cwe_create_copy_simulation.cpp
+++ cwe_guiWidgets/cwe_create_copy_simulation.cpp
@@ -40,6 +40,13 @@ void CWE_Create_Copy_Simulation::linkDriver(VWTinterfaceDriver * theDriver)
 
 void CWE_Create_Copy_Simulation::on_pBtn_create_copy_clicked()
 {
+    if (myDriver->inOfflineMode())
+    {
+        myDriver->setCurrentCase(new CFDcaseInstance(selectedTemplate, myDriver));
+        myDriver->getMainWindow()->switchToParameterTab();
+        return;
+    }
+
     /* take emergency exit if nothing has been selected */
     FileTreeNode * selectedNode = ui->primary_remoteFileTree->getSelectedNode();
     if (selectedNode == NULL) { return; }
