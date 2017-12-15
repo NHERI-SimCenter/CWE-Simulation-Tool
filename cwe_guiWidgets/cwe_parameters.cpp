a0bdf2e6dbdcd4b3fe11de9b56c0a03457be6355
Peter Mackenzie-Helnwein <mackheln@gmail.com>
2017-12-15 12:53:13 -0800

    extra debug chatter

--- cwe_guiWidgets/cwe_parameters.cpp
+++ cwe_guiWidgets/cwe_parameters.cpp
@@ -49,6 +49,9 @@ void CWE_Parameters::on_pbtn_saveAllParameters_clicked()
 
 void CWE_Parameters::saveAllParams()
 {
+    qDebug() << ui->theTabWidget->collectParamData();
+
+
     CFDcaseInstance * linkedCFDCase = myDriver->getCurrentCase();
     if (linkedCFDCase != NULL)
     {
