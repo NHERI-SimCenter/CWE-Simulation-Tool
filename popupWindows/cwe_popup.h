189efec6705cd2d71bf68b8d4f162bebf364b25a
Peter Sempolinski <sempolin@bc.edu>
2018-04-09 02:43:16 -0400

    Cleaned up management of case instance objects

---
+++ popupWindows/cwe_popup.h
@@ -0,0 +1,17 @@
+#ifndef CWE_POPUP_H
+#define CWE_POPUP_H
+
+#include <QMainWindow>
+
+class cwe_popup : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit cwe_popup(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CWE_POPUP_H
