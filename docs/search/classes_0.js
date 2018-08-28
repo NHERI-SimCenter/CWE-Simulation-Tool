d0053e00eac78d8b206a84bd003bfee7a6fa0178
Peter Sempolinski <psempoli@users.noreply.github.com>
2018-08-28 12:58:27 -0400

    Moved help docs around to be hostable on github

--- docs/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cwe_5fglobals',['cwe_globals',['../classcwe__globals.html',1,'']]],
-  ['cwe_5finterfacedriver',['CWE_InterfaceDriver',['../classCWE__InterfaceDriver.html',1,'']]]
-];
