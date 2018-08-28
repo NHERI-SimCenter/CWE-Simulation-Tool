27b1cd98fd3bac2f60195c211191e4949527d539
Peter Sempolinski <psempoli@users.noreply.github.com>
2018-08-28 12:59:33 -0400

    Re-placed CWE oxygen blank

---
+++ docs/internal/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "c",
+  1: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
